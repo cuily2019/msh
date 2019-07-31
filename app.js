9f64332db2e224a764f7feba3e9a718c15242451
msh <2356575727@qq.com>
2019-07-31 08:24:49 +0800

    second

--- app.js
+++ app.js
@@ -23,15 +23,53 @@ var pool=mysql.createPool({
 });
 //添加路由
 //1.用户注册
-server.get('/zhuce',function(req,res){
-	var obj=req.query;
-	pool.query("INSERT INTO foot_user SET ?",[obj],function(err,result){
-		if (err)
-		{
-			throw err
+server.post('/zhuce',function(req,res){
+	var obj=req.body;
+	console.log(obj)
+	pool.query("SELECT * from foot_user where fname=?",[obj.fname],function(err,result){
+		if(err)throw err
+		if(result.length!=0){
+			res.send("用户名已被占用")
+			return
+		}else{
+			if(!obj.fname){
+				res.send("用户名不能为空")
+				return;
+			}else if(obj.fname.length<6||obj.fname.length>12){
+				res.send("用户名用户为6~12位数字和字母组成");
+				return;
+			}
+		
+			if(!obj.fpwd){
+				res.send("密码不能为空")
+				return
+			}else if(obj.fpwd.length<6||obj.fpwd.length>12){
+			   res.send("密码为6~12位数字和字母组成");
+			}
+			
+			if(obj.fpwd2!=obj.fpwd){
+				res.send("两次密码不一致")
+				return
+			}
+		
+			if(!obj.phone){
+				res.send("手机号码不能为空")
+				return
+			}else if(obj.phone.length!=11){
+				res.send("请输入正确手机号码")
+				return
+			}
+		
+			pool.query("INSERT INTO foot_user VALUES(null,?,md5(?),?)",[obj.fname,obj.fpwd,obj.phone],function(err,result){
+				if (err)
+				{
+					throw err
+				}
+				res.send("注册成功")
+			})
 		}
-		console.log(result)
 	})
+	
 });
 //首页商品
 server.get("/product",function(req,res){
@@ -54,15 +92,26 @@ server.get("/query",function(req,res){
 		if(err){
 			throw err
 		}
+		console.log(result)
 		if (result.length>0){
 			res.send(result)
 		}
 	})
 })
-////用户登录
-//server.get("/denglu",function(req,res){
-//	var obj=req.query;
-//	pool.query("SELECT fid FROM foot_user where fname=?",[],function(req,res){
-//
-//	})
-//})
+//用户登录
+server.post("/denglu",function(req,res){
+	var obj=req.body;
+	console.log(obj)
+	pool.query("SELECT fid FROM foot_user where fname=? and fpwd=md5(?)",[obj.fname,obj.fpwd],function(err,result){
+		if(err)throw err
+		if(result.length==0){
+			res.send("用户名或密码错误")
+		}else{
+			res.send("登陆成功")
+		}
+	})
+})
+
+
+
+
